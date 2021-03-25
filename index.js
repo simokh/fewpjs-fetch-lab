828c6af60b67d1294ea4272e8dc13f91d4973cb8
simokh <m.simokhouili@gmail.com>
2021-03-24 23:37:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books') .then(function(response) {
+  return response.json();
+}).then(json => renderBooks(json));
   
 }
 
@@ -15,3 +17,6 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+
